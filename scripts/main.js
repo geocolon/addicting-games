2d9c71f88e41667ee7b91732af018db05d9bd4d9
George Colon <geocolon@gmail.com>
2018-11-28 12:36:31 -0800

    updating main body

--- scripts/main.js
+++ scripts/main.js
@@ -19,30 +19,3 @@
 //   // Configuration options go here
 //   options: {}
 // });
-
-
-$('.center').slick({
-  centerMode: true,
-  centerPadding: '60px',
-  slidesToShow: 3,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 3
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 1
-      }
-    }
-  ]
-});
--- scripts/tab-menu.js
+++ scripts/tab-menu.js
@@ -1,23 +1,26 @@
-function openPage(pageName,elmnt,color) {
-  let i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName('tabcontent');
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = 'none';
-  }
-  tablinks = document.getElementsByClassName('tablink');
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].style.backgroundColor = '';
-  }
-  document.getElementById(pageName).style.display = 'block';
-  elmnt.style.backgroundColor = color;
+var slideIndex = 1;
+showSlides(slideIndex);
 
-  let startBox = document.getElementById('select-player').style.display;
-    let startBtn = document.getElementById('defaultOpen').style;
-    if(startBox === 'block'){
-        console.log(startBtn.color = 'black');
-    } else {
-        startBtn.color = 'white';
-    }
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
-// Get the element with id="defaultOpen" and click on it
-document.getElementById('defaultOpen').click();
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
