c8583d603d320281d61b6644468e1019bde1d0be
George Colon <geocolon@gmail.com>
2018-11-28 11:42:05 -0800

    Changing interface

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // var ctx = document.getElementById('myChart').getContext('2d');
 // var chart = new Chart(ctx, {
 //   // The type of chart we want to create
@@ -17,3 +19,30 @@
 //   // Configuration options go here
 //   options: {}
 // });
+
+
+$('.center').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
