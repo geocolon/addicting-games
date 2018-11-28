fd86bc25ca8ecad19836c7f8225b5c534ea1d9d3
George Colon <geocolon@gmail.com>
2018-11-28 04:19:29 -0800

    Done menu tabs

--- scripts/tab-menu.js
+++ scripts/tab-menu.js
@@ -1,5 +1,5 @@
 function openPage(pageName,elmnt,color) {
-  var i, tabcontent, tablinks;
+  let i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName('tabcontent');
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = 'none';
@@ -11,6 +11,13 @@ function openPage(pageName,elmnt,color) {
   document.getElementById(pageName).style.display = 'block';
   elmnt.style.backgroundColor = color;
 
+  let startBox = document.getElementById('select-player').style.display;
+    let startBtn = document.getElementById('defaultOpen').style;
+    if(startBox === 'block'){
+        console.log(startBtn.color = 'black');
+    } else {
+        startBtn.color = 'white';
+    }
 }
 // Get the element with id="defaultOpen" and click on it
 document.getElementById('defaultOpen').click();
