b81f9a698a5b1404ccfe09a427aa14c708791693
George Colon <geocolon@gmail.com>
2018-11-30 14:15:32 -0800

    Skill Bar not working

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,29 @@
 'use strict';
 
 
+
 $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
+  loop:true,
+  margin:10,
+  nav:true,
+  responsive:{
+    0:{
+      items:1
+    },
+    600:{
+      items:3
+    },
+    1000:{
+      items:5
     }
-})
+  }
+});
+// https://codepen.io/MTushar/pen/GoNxOe
+
+jQuery(document).ready(function(){
+  jQuery('.skillbar').each(function(){
+    jQuery(this).find('.skillbar-bar').animate({
+      width:jQuery(this).attr('data-percent')
+    },6000);
+  });
+});
